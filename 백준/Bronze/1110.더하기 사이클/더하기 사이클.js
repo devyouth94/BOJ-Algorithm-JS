c13bacb7db7fe629c32d7f9628f47dc71af6b0b0
qimyoungzin <97172050+devyouth94@users.noreply.github.com>
2022-02-08 21:48:34 +0900

    [Bronze I] Title: 더하기 사이클, Time: 132 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/1110.더하기 사이클/더하기 사이클.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let input = fs.readFileSync('dev/stdin').toString();
+
+let origin = Number(input);
+let count = 0;
+
+while (true) {
+	let sum = Math.floor(input/10) + (input%10);
+	input = (input%10)*10 + (sum%10);
+	
+	count++
+	
+	if ( origin == input ) break;
+}
+
+console.log( count );
