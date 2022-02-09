60fa4b9d0a2abc2204c9cf99a84c7f3538f84cf8
qimyoungzin <97172050+devyouth94@users.noreply.github.com>
2022-02-09 22:58:19 +0900

    [Bronze III] Title: 최소, 최대, Time: 444 ms, Memory: 94444 KB -BaekjoonHub

--- 백준/Bronze/10818.최소， 최대/최소， 최대.js
+++ 백준/Bronze/10818.최소， 최대/최소， 최대.js
@@ -1,20 +1,9 @@
 const fs = require('fs');
 const input = fs.readFileSync('dev/stdin').toString().split('\n');
 
-let N = Number(input[0]);
-let integer = input[1].split(' ').map(Number);
+let numbers = input[1].split(' ').map(Number);
 
-let min = integer[0];
-let max = integer[0];
-
-for ( let i = 0; i < N; i++ ) {
-	if ( min > integer[i] ) {
-		min = integer[i];
-	}
-	
-	if ( max < integer[i] ) {
-		max = integer[i];
-	}
-}
+let min = Math.min(...numbers);
+let max = Math.max(...numbers);
 
 console.log(`${min} ${max}`);
