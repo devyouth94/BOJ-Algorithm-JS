de4cf50f50240d8c2aa65c3f6d94f413950835d0
qimyoungzin <devyouth94@gmail.com>
2022-07-16 01:31:05 +0900

    [level 1] Title: 2016년, Time: 0.26 ms, Memory: 30.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,20 +1,6 @@
 function solution(a, b) {
-    var answer = '';
     
-    let count = -1;
-
-const monthArray = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-const weeksArray = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
-
-
-for(let i = 0; i < a-1; i++) {
-  count += monthArray[i];
-};
-
-count += Number(b);
-
-let day = count % 7;
-answer = weeksArray[day];
+    let answer = new Date(2016, a-1, b).toString().slice(0, 3).toUpperCase();
     
     return answer;
 }
