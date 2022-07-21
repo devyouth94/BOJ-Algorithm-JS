061fffa22e5f223dc1bfada4ad9f1a7feed5b1c3
qimyoungzin <devyouth94@gmail.com>
2022-07-21 14:29:55 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.06 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,5 +1,3 @@
-function solution(seoul) {
-    let location = seoul.indexOf("Kim");
-        
-    return `김서방은 ${location}에 있다`;
+function solution(seoul) {        
+    return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
 }
